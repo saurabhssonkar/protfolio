505a8211fb37a4414cbfc39620fd960dfc03fc7c
saurabhssonkar <saurabhssonkar@gmail.com>
2023-04-04 01:38:14 -0400

    finial project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillYoutube,
+  AiOutlineArrowUp,
+} from "react-icons/ai";
+import logo from "../assets/logo0.jpg"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <img
+          src={logo}
+          alt="Founder"
+        />
+
+        <h2>Saurabh Sonkar</h2>
+        <p>Motivation is temporary, but discipline last forever.</p>
+      </div>
+
+      <aside>
+        <h2>Social Media</h2>
+
+        <article>
+          <a href="https://youtube.com" target={"blank"}>
+            <AiFillYoutube />
+          </a>
+          <a href="https://instagram.com/saura_bh9076" target={"blank"}>
+            <AiFillInstagram />
+          </a>
+          <a href="https://github.com/saurabhssonkar" target={"blank"}>
+            <AiFillGithub />
+          </a>
+        </article>
+      </aside>
+      <a href="#home">
+        <AiOutlineArrowUp />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,115 @@
+import React, { useRef } from "react";
+import { animate, motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+import { BsArrowUpRight, BsChevronDown } from "react-icons/bs";
+import me from "../assets/logo12.png";
+
+const Home = ({ ratio }) => {
+  const clientCount = useRef(null);
+  const projectCount = useRef(null);
+
+  const animationClientsCount = () => {
+    animate(0, 1000, {
+      duration: 1,
+      onUpdate: (v) => (clientCount.current.textContent = v.toFixed()),
+    });
+  };
+  const animationProjectsCount = () => {
+    animate(0, 10, {
+      duration: 1,
+      onUpdate: (v) => (projectCount.current.textContent = v.toFixed()),
+    });
+  };
+
+  const animations = {
+    h1: {
+      initial: {
+        x: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        x: 0,
+        opacity: 1,
+      },
+    },
+    button: {
+      initial: {
+        y: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        y: 0,
+        opacity: 1,
+      },
+    },
+  };
+  return (
+    <div id="home">
+      <section>
+        <div>
+          <motion.h1 {...animations.h1}>
+            Hi, I Am <br /> Saurabh Sonkar
+          </motion.h1>
+
+          <Typewriter
+            options={{
+              strings: ["A Developer", "A Designer", "A Creator"," A Software Engineer"],
+              autoStart: true,
+              loop: true,
+              cursor: "",
+              wrapperClassName: "typewriterpara",
+            }}
+          />
+
+          <div>
+            <a href="mailto:official.6packprogrammer@gmail.com">Hire Me</a>
+            <a href="#work">
+              Projects <BsArrowUpRight />
+            </a>
+          </div>
+
+          <article>
+            <p>
+              +
+              {ratio < 2 && (
+                <motion.span
+                  whileInView={animationClientsCount}
+                  ref={clientCount}
+                ></motion.span>
+              )}
+            </p>
+            <span>Data Structre</span>
+          </article>
+
+          <aside>
+            <article>
+              <p>
+                +
+                {ratio < 2 && (
+                  <motion.span
+                    ref={projectCount}
+                    whileInView={animationProjectsCount}
+                  >
+                    600
+                  </motion.span>
+                )}
+              </p>
+              <span>Projects Done</span>
+            </article>
+
+            <article data-special>
+              <p>Contact</p>
+              <span>saurabhssonkar@gmail.com</span>
+            </article>
+          </aside>
+        </div>
+      </section>
+      <section>
+        <img src={me} alt="Saurabh" />
+      </section>
+      <BsChevronDown />
+    </div>
+  );
+};
+
+export default Home;
